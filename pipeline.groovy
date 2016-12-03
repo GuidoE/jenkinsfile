02fac123eb638f92002f0d99075098813bb72d78
jsundqvist <johannes@codecatalyst.se>
2016-12-03 23:42:01 +0100

    stage blocks

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,35 +1,35 @@
 #!groovy
 { ->
 	node {
-		stage 'build'
-		checkout scm
-		sh './gradlew clean build -x test -x check'
+		stage 'build' {
+			checkout scm
+			sh './gradlew clean build -x test -x check'
+		}
 	
-		stage 'test'
-		try {
-			sh './gradlew test'
-		} finally {
+		stage 'test' {
 			try {
+				sh './gradlew test'
+			} finally {
 				step([$class: 'JUnitResultArchiver', testResults: '**/build/test-results/test/TEST-*.xml', allowEmptyResults: true])
 				step([$class: 'JacocoPublisher'])
-			} catch(e) {
-				println e	
 			}
 		}
 
-		stage 'check'
-		try {
-			sh './gradlew check --continue'
-		} catch(e) {
-			currentBuild.result = "UNSTABLE"
-		} finally {
-			step([$class: 'CheckStylePublisher', pattern: "build/reports/checkstyle/main.xml"])
-			step([$class: 'FindBugsPublisher', pattern: "build/reports/findbugs/main.xml"])
-			step([$class: 'PmdPublisher', pattern: "build/reports/pmd/main.xml"])
+		stage 'check' {
+			try {
+				sh './gradlew check --continue'
+			} catch(e) {
+				currentBuild.result = "UNSTABLE"
+			} finally {
+				step([$class: 'CheckStylePublisher', pattern: "build/reports/checkstyle/main.xml"])
+				step([$class: 'FindBugsPublisher', pattern: "build/reports/findbugs/main.xml"])
+				step([$class: 'PmdPublisher', pattern: "build/reports/pmd/main.xml"])
+			}
 		}
 
-		stage 'upload'
-		if(env.BRANCH_NAME == "master")
-			sh './gradlew uploadArchives'
+		stage 'upload' {
+			if(env.BRANCH_NAME == "master")
+				sh './gradlew uploadArchives'
+		}
 	}
 }
